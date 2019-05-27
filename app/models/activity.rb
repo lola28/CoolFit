b74a62be7f6f5ba25371dc15f30f9a78c63b1cbc
alice de chalendar <alice.dechalendar@gmail.com>
2019-05-27 17:11:24 +0100

    fixed seeding and schema

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :category
+  belongs_to :user
   has_many :interests, through: :users
   has_many :bookings, through: :users
 
@@ -7,7 +8,7 @@ class Activity < ApplicationRecord
   validates :location, presence: true
   validates :time, presence: true
   validates :duration, presence: true
-  validates :price, presence: true, default: nil
+  validates :price, default: nil
   validates :description, presence: true
   validates :photo_db, presence: true, unless: :photo_user?
   validates :photo_user, presence: true, unless: :photo_db?
