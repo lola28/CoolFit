bff67e9c15706f5687e5be7b3cfe078c014ff8bd
Frankie Ling <ingleson.fl@gmail.com>
2019-05-27 14:57:17 +0100

    added controllers and routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  validates :ratings, default: nil
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,3 @@ class User < ApplicationRecord
 end
 
 
-# reate_table "users", force: :cascade do |t|
-#     t.string "username"
-#     t.string "email"
-#     t.string "encrypted_password"
-#     t.integer "age"
-#     t.string "physical_activity"
-#     t.text "description"
-#     t.boolean "professional"
-#     t.string "first_name"
-#     t.string "last_name"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
