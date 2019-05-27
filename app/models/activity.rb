1eee37898e0c19633c8620df03c621a7a6132ce4
alice de chalendar <alice.dechalendar@gmail.com>
2019-05-27 16:03:14 +0100

    first elements of grid design + fixed activity price column and added name

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,12 +3,12 @@ class Activity < ApplicationRecord
   has_many :interests, through: :users
   has_many :bookings, through: :users
 
+  validates :name, presence: true
   validates :location, presence: true
   validates :time, presence: true
   validates :duration, presence: true
-  validates :price, presence: true, default: 0
+  validates :price, presence: true, default: nil
   validates :description, presence: true
   validates :photo_db, presence: true, unless: :photo_user?
   validates :photo_user, presence: true, unless: :photo_db?
 end
-
