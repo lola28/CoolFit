22e0e69a2e9070d83897022a72f20e9f50b04938
vladimir deziegler <vladimirdeziegler@gmail.com>
2019-05-28 10:39:19 +0100

    new page reviewed

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,7 +6,7 @@ class Activity < ApplicationRecord
   validates :location, presence: true
   validates :time, presence: true
   validates :duration, presence: true
-  validates :price, presence: true, default: 0
+  # validates :price, presence: true, default: 0
   validates :description, presence: true
   validates :photo_db, presence: true, unless: :photo_user?
   validates :photo_user, presence: true, unless: :photo_db?
